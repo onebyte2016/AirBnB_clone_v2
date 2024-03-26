ce3592c3e10efc73798d7df1187b0b68f43c1f8b
onebyte2016 <onebyte2016@gmail.com>
2024-03-26 10:19:42 -0700

    flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-from flask import Flask
+"""
+start Flask application
+"""
 
-# Create a Flask application
+from flask import Flask
 app = Flask(__name__)
 
-# Define routes
+
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def show_c(text):
-    # Replace underscores with spaces in the text
-    text = text.replace('_', ' ')
-    return f"C {text}"
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-# Run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port='5000')
