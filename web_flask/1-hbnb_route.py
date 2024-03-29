7e0d4ca7007c9ffa405fa06abbaa057a750f8a5f
onebyte2016 <onebyte2016@gmail.com>
2024-03-29 08:41:57 -0700

    hnbn rout

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,11 +5,13 @@ start Flask application
 from flask import Flask
 app = Flask(__name__)
 
+
 # Define routes
 @app.route('/', strict_slashes=False)
 def index():
     """Return Hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
